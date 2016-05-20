d7235fdff2913576ca496cb7c40bfe84ae74b1ae
Kurt Beheydt <kurt@postduif.be>
2016-05-20 10:26:38 +0200

    basic movement (forward)

--- arduino/robotreporter/robotreporter.ino
+++ arduino/robotreporter/robotreporter.ino
@@ -1,23 +1,27 @@
 /*
- HC-SR04 Ping distance sensor:
+ 2 HC-SR04 Ping distance sensor:
  VCC to 5v 
  GND to GND
  Echo to pinRightProxSensorEcho/pinLeftProxSensorEcho
  Trig to pinRightProxSensorTrig/pinLeftProxSensorTrig
 
- IRS01a sensors https://www.pololu.com/product/959
+ 2 IRS01a sensors https://www.pololu.com/product/959
  Vin to 5v
  GND to GND
  Out to pinLeftEdgeSensor/pinRightEdgeSensor
- */
 
+ L293D chip http://www.instructables.com/id/Control-your-motors-with-L293D-and-Arduino
+ motorA: pinDirectionA, pinSpeedA
+ motorB: pinDirectionB, pinSpeedB
+ 
+ */
 
 boolean sensorOnEdge (uint8_t pin);
 boolean sensorProximity(uint8_t trigPin, uint8_t echoPin);
 
 #define pinRightEdgeSensor 4             
 #define pinLeftEdgeSensor 5
-#define edgeSensorThreshold 100
+#define edgeSensorThreshold 100 // value of distance to ground to avoid to fall
 
 #define pinRightProxSensorEcho 6
 #define pinRightProxSensorTrig 7             
@@ -30,10 +34,19 @@ boolean sensorProximity(uint8_t trigPin, uint8_t echoPin);
 #define pinSpeedA 3
 #define pinSpeedB 11
 
+#define MOVE_FORWARD LOW
+#define MOVE_BACKWARD HIGH
+#define TURN_LEFT LOW
+#define TURN_RIGHT HIGH
+#define TURN_DURATION 1500000 // 2 secs
+
+#define ACTION_MOVING 1
+#define ACTION_TURNING 2
+#define ACTION_ASKINGQUESTION 3
+
+byte action;
 
 #define DEBUG true
-
-
 
 void setup() {
   pinMode(pinRightProxSensorTrig, OUTPUT);
@@ -41,16 +54,63 @@ void setup() {
   pinMode(pinLeftProxSensorTrig, OUTPUT);
   pinMode(pinLeftProxSensorEcho, INPUT);
 
+  pinMode(pinDirectionA, OUTPUT);
+  pinMode(pinDirectionB, OUTPUT);
+  pinMode(pinSpeedA, OUTPUT);
+  pinMode(pinSpeedB, OUTPUT);
+
   if (DEBUG) {
     Serial.begin(9600);
   }
-  
+
+  action = ACTION_MOVING;
   delay(500);
 
 }
 
 void loop() {
 
+  if ((action == ACTION_MOVING) || (action == ACTION_TURNING)) {
+    obstacleCheck();
+  }
+
+  switch (action) {
+    case ACTION_MOVING:
+        moveWheels(MOVE_FORWARD);
+      break;
+
+    case ACTION_TURNING:
+
+      break;
+
+    case ACTION_ASKINGQUESTION:
+
+      break;
+  }
+
+  delay(50);
+}
+
+
+void moveWheels(uint8_t dir)
+{
+  stopWheels();
+  int velocity = 220;
+  
+  digitalWrite(pinDirectionA, dir);
+  digitalWrite(pinDirectionB, dir);
+  analogWrite(pinSpeedA, velocity);
+  analogWrite(pinSpeedB, velocity);
+}
+
+void stopWheels()
+{
+  analogWrite(pinSpeedA, 0);
+  analogWrite(pinSpeedB, 0);
+}
+
+void obstacleCheck()
+{
   // Checkin for obstacles
   if (sensorOnEdge(pinLeftEdgeSensor)) {
     printDebugText("LEFT OVER EDGE");
@@ -61,9 +121,8 @@ void loop() {
   } else if (sensorProximity(pinRightProxSensorTrig, pinRightProxSensorEcho)) {
     printDebugText("Possible collision right");
   }
-
-  delay(50);
 }
+
 
 /**
 * returns true if the proximity sensor is too close
@@ -76,7 +135,7 @@ boolean sensorProximity(uint8_t trigPin, uint8_t echoPin)
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  
+
   duration = pulseIn(echoPin, HIGH);
 
   //Calculate the distance (in cm) based on the speed of sound.
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # robotreporter
 
 A robot controlled via Facebook
+
+gcode files are for K8400 Vertex 3D printer http://www.vertex3dprinter.eu/
